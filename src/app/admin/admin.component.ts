f687aa6ddc5ed4b98dac9c30fc3960ad90bc91e5
GORGUI\gorgu <gorguindongondiaye@gmail.com>
2025-05-19 20:51:47 -0400

    correction bug3

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,43 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-admin',
   standalone: false,
-  
+
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
-export class AdminComponent implements OnInit{
+export class AdminComponent implements OnInit {
 
   drawerMode: 'side' | 'over' = 'side';
   drawerOpened = true;
-  
-  
-  constructor(public authService:AuthenticationService,private breakpointObserver: BreakpointObserver){
+  isSmallScreen = false;
 
+
+  constructor(
+    public authService: AuthenticationService,
+    private breakpointObserver: BreakpointObserver
+  ) {
     this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet])
-    .subscribe(result => {
-      if (result.matches) {
-        this.drawerMode = 'over';
-        this.drawerOpened = false;
-      } else {
-        this.drawerMode = 'side';
-        this.drawerOpened = true;
-      }
-    });
-}
-
-toggleDrawer(drawer: any) {
-  drawer.toggle();
-}
-  ngOnInit(): void {
-    
+      .subscribe(result => {
+        if (result.matches) {
+          this.drawerMode = 'over';
+          this.drawerOpened = false;
+          this.isSmallScreen = true;
+        } else {
+          this.drawerMode = 'side';
+          this.drawerOpened = true;
+          this.isSmallScreen = false;
+        }
+      });
   }
 
-  logout(){
+
+  toggleDrawer(drawer: any) {
+    drawer.toggle();
+  }
+  ngOnInit(): void {
+
+  }
+
+  logout() {
     this.authService.logout();
   }
+
+  closeDrawerIfMobile(drawer: MatDrawer): void {
+    if (this.isSmallScreen) {
+      drawer.close();
+    }
+  }
+
 
 }
